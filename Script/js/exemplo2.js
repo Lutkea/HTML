b086782ebb608f846855abbc9cc67573ec5246c7
Amanda Lutke <53452692+Lutkea@users.noreply.github.com>
2020-09-14 22:01:58 -0300

    exemplos da aula - 14/09

---
+++ Script/js/exemplo2.js
@@ -0,0 +1,47 @@
+        // define o objeto
+        var estudante = {
+            nome: "Amanda",
+            idade: "19", 
+            matricula: "1234567"
+        };
+        
+        var estudante2 = {
+            nome: "José",
+            idade: "23",
+            matricula: "7654321"
+        }
+
+        // modificando propriedades
+        estudante2.idade = "24";
+
+        console.log(estudante2.nome);
+        
+        function carregou(){
+            alert("Oi");
+        }
+        function alterou(){
+            alert("Alterou");
+        }
+        function salvarNome(){
+            var inputNome = document.getElementById("nome");
+            var valorNome = inputNome.value;
+            if(valorNome == "Amanda")
+                inputNome.style.backgroundColor = "green";
+            else
+                inputNome.style.backgroundColor = "red";
+            //alert(valorNome);
+            var divBox = document.getElementById("box");
+ 
+           // divBox.innerHTML = "<p>"+valorNome+"</p>";
+ 
+            var paragrafo = document.createElement("div");
+            paragrafo.innerText = valorNome;
+            paragrafo.style.backgroundColor = "pink";
+            divBox.appendChild(paragrafo);
+ 
+            debugger;
+        }
+        function limparConteudo(){
+            var divBox = document.getElementById("box");
+            divBox.innerHTML = "";
+        }
